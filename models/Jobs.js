a50c04c173b8662afc5ee23bd43ab2b73b5a0382
Zach Stone <zachinjapan888@gmail.com>
2022-01-30 07:34:12 -0500

    made job schema

---
+++ models/Jobs.js
@@ -0,0 +1,39 @@
+import moongose from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company"],
+      maxlength: 20,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "remote", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+      default: "my location",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamp: true }
+);
+
+export default mongoose.model("Job", JobSchema);
