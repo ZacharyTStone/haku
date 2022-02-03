537498f3707088b03b50eefc1fb732e1db18cea6
Zach Stone <zachinjapan888@gmail.com>
2022-02-03 16:12:21 -0500

    fix profile.js imports

--- client/src/pages/Dashboard/Profile.js
+++ client/src/pages/Dashboard/Profile.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { FormRow, Alert } from "../../components";
+import { FormRow } from "../../components";
+import { Alert } from "../../components";
 import { useAppContext } from "../../context/appContext";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 const Profile = () => {
