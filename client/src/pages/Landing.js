ac042db1a184d7bb36caada6e69c872ed9c8107f
Zach Stone <zachinjapan888@gmail.com>
2022-12-17 18:37:12 -0500

    meta policy

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -63,7 +63,7 @@ const Landing = () => {
    
      ></script>
        // cors policy
-     <meta http-equiv="Content-Security-Policy"  content="connect-src https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2495439414515461" />
+     <meta http-equiv="Content-Security-Policy"  content="script-src https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2495439414515461;" />
       </Helmet>
       {/* <ToastContainer
         position="top-right"
