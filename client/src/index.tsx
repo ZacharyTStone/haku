669abc5825691e08bbd86eda89461433a542c63e
Zach Stone <83141990+ZacharyTStone@users.noreply.github.com>
2023-12-19 15:22:23 -0500

    fix build issue due to unused import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "normalize.css";
 import "./assets/css/index.css";
 import App from "./App";
-import "./translations/i18n.js";
 import { AppProvider } from "./context/appContext";
 
 const root = ReactDOM.createRoot(
